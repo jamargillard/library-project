8a8894e38bb66acb72fcbb5c70e4bd281afa93fe
jamar gillard <jaydevelopment101@gmail.com>
2023-09-10 15:17:58 -0400

    need to fix landing pg

--- src/App.js
+++ src/App.js
@@ -52,8 +52,8 @@ setCart(cart.filter(book => book.id !== item.id))
     <Router> 
     <div className="App">
 <Nav numberOfItems={numberOfItems()} />
-<Home></Home>
-<Route path='*/home' exact component={Home}  />
+<Route path='/' from='/' to='home'  />
+<Route path='/home' exact component={Home}  />
 <Route path='/Books' exact render={()=> <Books books ={books} />} />
 <Route path='/Books/:id' exact render={()=> <BookInfo books ={books} addToCart={addToCart} cart={cart}/>} />
 <Route path='/cart'  render={()=> <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem} />} />
